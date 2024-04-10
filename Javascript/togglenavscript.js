51505e5a6b50ff49964e3080e65ee25ebf7dcdbb
jankarong <jankarong@gmail.com>
2024-04-10 10:54:42 +0800

    Update togglenavscript.js
    add comment for toggle navigation javascript

--- Javascript/togglenavscript.js
+++ Javascript/togglenavscript.js
@@ -1,13 +1,20 @@
+
+
+// Wait for the web page to be fully ready, then  run the function.
 $(document).ready(function () {
+    //  When an element with the 'toggle-menu' class is clicked， run the function.
     $('.toggle-menu').click(function () {
+        // Find elements with the 'navigation-menu' class and switch their visibility
         $('.navigation-menu').toggle();
     });
 
-
+    //When the size of the browser window changes,then run the function.
     $(window).resize(function () {
+        //If the window is wider than 768 px
         if ($(window).width() > 768) {
-
+            //show elements with the 'navigation-menu' class
             $('.navigation-menu').show();
+            //else do nothing
         } else {
 
         }
